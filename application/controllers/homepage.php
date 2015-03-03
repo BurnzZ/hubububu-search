9fb059f26477c2064bc816ea4d42d85da7c099a2
Kevin Lloyd H. Bernal <kevinoxy@gmail.com>
2015-03-03 12:38:17 +0800

    removed SQL dumps; added psuedocodes of algo in comments

--- application/controllers/homepage.php
+++ application/controllers/homepage.php
@@ -17,6 +17,28 @@ class Homepage extends CI_Controller {
 		$this->load->model('search_algo');
         $data['algo_1'] = $this->search_algo->get_algo_1($data['query']);
 
+        /*
+
+        // COMPUTATION HERE
+
+        $algo_1 = array()
+
+        $algo_1['string'] = ["justin beiber", "cat mouse bieber"];
+        $algo_1['score'] = [2.0, 1.6666]
+
+        $data['algo_1'] = $algo_1
+
+        // COMPUTATION HERE
+
+        $algo_2 = array()
+
+        $algo_2['string'] = ["justin beiber", "cat mouse bieber"];
+        $algo_2['score'] = [2.0, 1.6666]
+
+        $data['algo_2'] = $algo_2
+
+        */
+
         // returns back AJAX results
 		echo json_encode($data);
 	}
