18280236f00b3c4e8e1249ab34b8f5fa3cd9a0ac
budjako <budjako@gmail.com>
2015-03-08 19:59:48 +0800

    Fix result print

--- application/controllers/homepage.php
+++ application/controllers/homepage.php
@@ -17,26 +17,29 @@ class Homepage extends CI_Controller {
 		$this->load->model('search_algo');
         $algo_1_result = $this->search_algo->get_algo_1($data['query']);
 
-        // COMPUTATION HERE
+        if($algo_1_result){
+            // COMPUTATION HERE
 
-        $algo_1 = array();
+            $algo_1 = array();
 
-        $algo_1['string'] = $algo_1_result[0];
-        $algo_1['score'] = $algo_1_result[1];
+            $algo_1['string'] = $algo_1_result[0];
+            $algo_1['score'] = $algo_1_result[1];
 
-        $data['algo_1'] = $algo_1;
+            $data['algo_1'] = $algo_1;
+        }
 
         // COMPUTATION HERE
 
         $algo_2_result = $this->search_algo->get_algo_2($data['query']);
 
-        $algo_2 = array();
+        if($algo_2_result){
+            $algo_2 = array();
 
-        $algo_2['string'] = $algo_2_result[0];
-        $algo_2['score'] = $algo_2_result[1];
+            $algo_2['string'] = $algo_2_result[0];
+            $algo_2['score'] = $algo_2_result[1];
 
-        $data['algo_2'] = $algo_2;
-
+            $data['algo_2'] = $algo_2;
+        }
         // returns back AJAX results
 		echo json_encode($data);
 	}
--- assets/js/main.js
+++ assets/js/main.js
@@ -13,13 +13,14 @@ $(document).ready( function() {
 
 			success: function(data) {
 
-				console.log(data.algo_2);
+				console.log(data);
 
 				$('#repeat-result').html("<span class='bold light right-space'>You have searched:</span>" + data.query);
 
-				if (data) {
+					console.log(jQuery.type( data.algo_1 ));
+				if ( jQuery.type( data.algo_1 ) === "object") {
 					/* Populate Algo 1 Results */
-
+					
 					var algo_1 = $('#algo-1 table tbody');
 					$(algo_1).html("");
 
@@ -28,8 +29,13 @@ $(document).ready( function() {
 					for (var i=0; i<data.algo_1.string.length; i++) {
 						$(algo_1).append("<tr><td>" + data.algo_1.string[i] + "</td><td>" + data.algo_1.score[i] + "</td></tr>");
 					}
+				}
 
+				else{
+					$('#algo-1 .num-results').html("Number of results: 0");
+				}
 
+				if ( jQuery.type( data.algo_2 ) === "object") {
 					/* Populate Algo 2 Results */
 
 					var algo_2 = $('#algo-2 table tbody');
@@ -42,13 +48,9 @@ $(document).ready( function() {
 					}
 
 				}
-
-				if (data.algo_1.length == 0)
-					$('#algo-1 .num-results').html("");
-
-				if (data.algo_2.length == 0)
-					$('#algo-2 .num-results').html("");
-				
+				else{
+					$('#algo-2 .num-results').html("Number of results: 0");
+				}
 			},
 			error: function(err) {
 				console.log(err);
