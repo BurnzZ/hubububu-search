7009c779cffd4e3325c90fba0a2bcbaf7328f43f
Kevin Lloyd H. Bernal <kevinoxy@gmail.com>
2015-03-09 17:48:14 +0800

    refactored code + added fallback for no results

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,17 @@
+/**
+*	@results - data
+*	@id 	 - id of container
+*/
+function populate(result, id) {
+	$(id + ' table tbody').html("");
+
+	$(id + ' .num-results').html("Number of results: " + result.string.length);
+
+	for (var i=0; i<result.string.length; i++) {
+		$(id + ' table tbody').append("<tr><td>" + result.string[i] + "</td><td>" + result.score[i] + "</td></tr>");
+	}
+}
+
 $(document).ready( function() {
 
 	$(".submit").click(function(event) {
@@ -13,12 +27,17 @@ $(document).ready( function() {
 
 			success: function(data) {
 
-				console.log(data);
-
 				$('#repeat-result').html("<span class='bold light right-space'>You have searched:</span>" + data.query);
 
+
+				// no results
+				if ( data.algo_1 === undefined) {
+					$('#results-container > div .num-results').html("Number of results: 0");
+					$('#results-container > div table tbody').html("");
+					return;
+				}
+
 				var words=data.query.split(" ");
-				console.log(words);
 
 				for(var j=0; j<words.length; j++){
 					for (var k=0; k<data.algo_1.string.length; k++) {
@@ -29,41 +48,8 @@ $(document).ready( function() {
 					}
 				}
 
-				if ( jQuery.type( data.algo_1 ) === "object") {
-					/* Populate Algo 1 Results */
-
-					var algo_1 = $('#algo-1 table tbody');
-					$(algo_1).html("");
-
-					$('#algo-1 .num-results').html("Number of results: " + data.algo_1.string.length);
-
-					for (var i=0; i<data.algo_1.string.length; i++) {
-						$(algo_1).append("<tr><td>" + data.algo_1.string[i] + "</td><td>" + data.algo_1.score[i] + "</td></tr>");
-					}
-				}
-
-				else{
-					$('#algo-1 .num-results').html("Number of results: 0");
-					$('#algo-1 table tbody').empty();
-				}
-
-				if ( jQuery.type( data.algo_2 ) === "object") {
-					/* Populate Algo 2 Results */
-
-					var algo_2 = $('#algo-2 table tbody');
-					$(algo_2).html("");
-
-					$('#algo-2 .num-results').html("Number of results: " + data.algo_2.string.length);
-
-					for (var i=0; i<data.algo_2.string.length; i++) {
-						$(algo_2).append("<tr><td>" + data.algo_2.string[i] + "</td><td>" + data.algo_2.score[i] + "</td></tr>");
-					}
-
-				}
-				else{
-					$('#algo-2 .num-results').html("Number of results: 0");
-					$('#algo-2 table tbody').empty();
-				}
+				populate(data.algo_1, '#algo-1');
+				populate(data.algo_2, '#algo-2');
 			},
 			error: function(err) {
 				console.log(err);
